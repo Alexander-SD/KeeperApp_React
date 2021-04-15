1c10638bd84b2c51100c4e007329aefb27c17506
Alexander <alexander@Darias-MacBook-Pro.local>
2021-04-15 15:53:24 +0200

    Add an express server. Build the App

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+const publicPath = path.join(__dirname, "..", "build");
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+    res.send(path.join(publicPath, "index.html"));
+    //res.send("../public/index.html");
+}); 
+
+
+
+
+
+app.listen(3000 , () => 
+console.log("Server is running on port 3000"))
+
