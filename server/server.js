617a54f026a68c60728b25c373d1635285fa0df0
Alexander <alexander@Darias-MacBook-Pro.local>
2021-04-19 13:23:53 +0200

    Change the port for deploying on heroku

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const app = express();
 
 const publicPath = path.join(__dirname, "..", "build");
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
@@ -15,6 +17,6 @@ app.get("*", (req, res) => {
 
 
 
-app.listen(3000 , () => 
-console.log("Server is running on port 3000"))
+app.listen(port , () => 
+console.log("Server is running on port " + port))
 
